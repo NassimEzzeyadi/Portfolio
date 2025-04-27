4dcec64ed926f8dfc48c0eee9bf3155eebfa6cd9
NassimEzzeyadi <116871162+NassimEzzeyadi@users.noreply.github.com>
2025-04-27 22:23:54 +0200

    Delete page.js

--- page.js
+++
@@ -1,8 +0,0 @@
- // Add your JavaScript here
-const toggle = document.querySelector('.toggle');
-
-toggle.addEventListener('click', function() {
-    
-document.body.classList.toggle('dark-mode');
-});
-    
